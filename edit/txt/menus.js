4fafe85e5189f13e7ad88f13a54374155f2dd592
Eric yui <yunokkj@gmail.com>
2022-02-06 07:36:37 -0300

    xy lindo

--- edit/txt/menus.js
+++ edit/txt/menus.js
@@ -4,694 +4,360 @@ var prefix = JSON.parse(fs.readFileSync('./edit/prefix.json'));
 const readmore = "͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏";
 
 exports.menuhentai = `
-┏━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙽𝚜𝚏𝚠
-┗━━━━━━━
-
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}ass 
-┃
-┃۞ ${prefix}bdsm 
-┃
-┃۞ ${prefix}blowjob 
-┃
-┃۞ ${prefix}cuckold 
-┃
-┃۞ ${prefix}cum 
-┃
-┃۞ ${prefix}cumslut 
-┃
-┃۞ ${prefix}doujin 
-┃
-┃۞ ${prefix}ero 
-┃
-┃۞ ${prefix}eroneko 
-┃
-┃۞ ${prefix}eroyuri 
-┃
-┃۞ ${prefix}feet 
-┃
-┃۞ ${prefix}femdom 
-┃
-┃۞ ${prefix}foxgirl 
-┃
-┃۞ ${prefix}futa 
-┃
-┃۞ ${prefix}gangbang 
-┃
-┃۞ ${prefix}glasses 
-┃
-┃۞ ${prefix}maid 
-┃
-┃۞ ${prefix}netorare 
-┃
-┃۞ ${prefix}orgy 
-┃
-┃۞ ${prefix}panties 
-┃
-┃۞ ${prefix}pussy 
-┃
-┃۞ ${prefix}rhentai
-┃
-┃۞ ${prefix}school 
-┃
-┃۞ ${prefix}solo 
-┃
-┃۞ ${prefix}succubus 
-┃
-┃۞ ${prefix}tentacles 
-┃
-┃۞ ${prefix}thighs 
-┃
-┃۞ ${prefix}trap 
-┃
-┃۞ ${prefix}ugly 
-┃
-┃۞ ${prefix}fantasy 
-┃
-┃۞ ${prefix}zettai 
-┗━━━━━━━━━━━━━
-
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *NSFW* ❳═>
+┃║⛇ *${prefix}ass*
+┃║⛇ *${prefix}bdsm*
+┃║⛇ *${prefix}blowjob*
+┃║⛇ *${prefix}cuckold*
+┃║⛇ *${prefix}cum*
+┃║⛇ *${prefix}cumslut*
+┃║⛇ *${prefix}doujin*
+┃║⛇ *${prefix}ero*
+┃║⛇ *${prefix}eroneko*
+┃║⛇ *${prefix}eroyuri*
+┃║⛇ *${prefix}feet*
+┃║⛇ *${prefix}femdom*
+┃║⛇ *${prefix}foxgirl*
+┃║⛇ *${prefix}futa*
+┃║⛇ *${prefix}gangbang*
+┃║⛇ *${prefix}glasses*
+┃║⛇ *${prefix}maid*
+┃║⛇ *${prefix}netorare 
+┃║⛇ *${prefix}orgy*
+┃║⛇ *${prefix}panties*
+┃║⛇ *${prefix}pussy*
+┃║⛇ *${prefix}rhentai*
+┃║⛇ *${prefix}school*
+┃║⛇ *${prefix}solo*
+┃║⛇ *${prefix}succubus*
+┃║⛇ *${prefix}tentacles*
+┃║⛇ *${prefix}thighs*
+┃║⛇ *${prefix}trap*
+┃║⛇ *${prefix}ugly*
+┃║⛇ *${prefix}fantasy*
+┃║⛇ *${prefix}zettai*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.galeria = `
-┏━━━━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙶𝚊𝚕𝚎𝚛𝚒𝚊
-┗━━━━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}loli 
-┃
-┃۞ ${prefix}neko 
-┃
-┃۞ ${prefix}ranime 
-┃
-┃۞ ${prefix}wallpaper 
-┃
-┃۞ ${prefix}yuri 
-┃
-┃۞ ${prefix}kimono
-┃
-┃۞ ${prefix}uwu
-┗━━━━━━━━━━━━━
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *GALERIA* ❳═>
+┃║⛇ *${prefix}loli*
+┃║⛇ *${prefix}neko*
+┃║⛇ *${prefix}ranime*
+┃║⛇ *${prefix}wallpaper*
+┃║⛇ *${prefix}yuri*
+┃║⛇ *${prefix}kimono*
+┃║⛇ *${prefix}uwu*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.menuadms = `
-┏━━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙰𝚍𝚖𝚒𝚗
-┗━━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}add 
-┃
-┃۞ ${prefix}ban 
-┃
-┃۞ ${prefix}antifake 
-┃
-┃۞ ${prefix}antilink 
-┃
-┃۞ ${prefix}antipalavrão
-┃
-┃۞ ${prefix}nsfw 
-┃
-┃۞ ${prefix}welkom  
-┃
-┃۞ ${prefix}contador 
-┃
-┃۞ ${prefix}checkativo 
-┃
-┃۞ ${prefix}topativos 
-┃
-┃۞ ${prefix}opentime 
-┃
-┃۞ ${prefix}closetime 
-┃
-┃۞ ${prefix}opengp 
-┃
-┃۞ ${prefix}closegp 
-┃
-┃۞ ${prefix}promote 
-┃
-┃۞ ${prefix}demote 
-┃
-┃۞ ${prefix}hidetag 
-┃
-┃۞ ${prefix}linkgp 
-┃
-┃۞ ${prefix}desc 
-┃
-┃۞ ${prefix}multiprefix
-┃
-┃۞ ${prefix}setdesc 
-┃
-┃۞ ${prefix}setname 
-┃
-┃۞ ${prefix}setpic 
-┃
-┃۞ ${prefix}roleta
-┃
-┃۞ ${prefix}mute
-┃
-┃۞ ${prefix}status
-┃
-┃۞ ${prefix}addblack
-┃
-┃۞ ${prefix}unblack
-┃
-┃۞ ${prefix}blacklist
-┃
-┃۞ ${prefix}addwhite 
-┃
-┃۞ ${prefix}delwhite 
-┃
-┃۞ ${prefix}whitelist 
-┃
-┃۞ ${prefix}delete
-┃
-┃۞ ${prefix}listadmin
-┗━━━━━━━━━━━━━
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *MENU GRUPO* ❳═>
+┃║⛇ *${prefix}add* <num>
+┃║⛇ *${prefix}ban* <num>
+┃║⛇ *${prefix}antifake*
+┃║⛇ *${prefix}antilink*
+┃║⛇ *${prefix}antipalavrão*
+┃║⛇ *${prefix}nsfw*
+┃║⛇ *${prefix}welkom*
+┃║⛇ *${prefix}contador* <num>
+┃║⛇ *${prefix}checkativo* <num>
+┃║⛇ *${prefix}topativos*
+┃║⛇ *${prefix}opentime*
+┃║⛇ *${prefix}closetime*
+┃║⛇ *${prefix}opengp*
+┃║⛇ *${prefix}closegp*
+┃║⛇ *${prefix}promote* <num>
+┃║⛇ *${prefix}demote* <num>
+┃║⛇ *${prefix}hidetag*
+┃║⛇ *${prefix}linkgp*
+┃║⛇ *${prefix}desc*
+┃║⛇ *${prefix}multiprefix*
+┃║⛇ *${prefix}setdesc* <text>
+┃║⛇ *${prefix}setname* <text>
+┃║⛇ *${prefix}setpic* <tag img>
+┃║⛇ *${prefix}roleta*
+┃║⛇ *${prefix}mute*
+┃║⛇ *${prefix}status*
+┃║⛇ *${prefix}addblack* <num>
+┃║⛇ *${prefix}unblack* <num>
+┃║⛇ *${prefix}blacklist*
+┃║⛇ *${prefix}addwhite* <num>
+┃║⛇ *${prefix}delwhite*
+┃║⛇ *${prefix}whitelist*
+┃║⛇ *${prefix}delete* <tag img>
+┃║⛇ *${prefix}listadmins
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.menudono = `
-┏━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙳𝚘𝚗𝚘
-┗━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}block 
-┃
-┃۞ ${prefix}unblock 
-┃
-┃۞ ${prefix}blocklist 
-┃
-┃۞ ${prefix}gplist 
-┃
-┃۞ ${prefix}pvlist 
-┃
-┃۞ ${prefix}seradm 
-┃
-┃۞ ${prefix}sermembro 
-┃
-┃۞ ${prefix}leave 
-┃
-┃۞ ${prefix}join 
-┃
-┃۞ ${prefix}recado 
-┃
-┃۞ ${prefix}on 
-┃
-┃۞ ${prefix}off 
-┃
-┃۞ ${prefix}antipv
-┃
-┃۞ ${prefix}blockcmd
-┃
-┃۞ ${prefix}unblockcmd
-┃
-┃۞ ${prefix}cmdlist
-┃
-┃۞ ${prefix}anotar
-┃
-┃۞ ${prefix}notas
-┗━━━━━━━━━━━━━
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *DONO* ❳═>
+┃║⛇ *${prefix}block* <num>
+┃║⛇ *${prefix}unblock* <num>
+┃║⛇ *${prefix}blocklist*
+┃║⛇ *${prefix}gplist*
+┃║⛇ *${prefix}pvlist*
+┃║⛇ *${prefix}seradm*
+┃║⛇ *${prefix}sermembro*
+┃║⛇ *${prefix}leave*
+┃║⛇ *${prefix}join*
+┃║⛇ *${prefix}recado*
+┃║⛇ *${prefix}on*
+┃║⛇ *${prefix}off*
+┃║⛇ *${prefix}antipv* <on/off>
+┃║⛇ *${prefix}blockcmd* <cmd>
+┃║⛇ *${prefix}unblockcmd* <cmd>
+┃║⛇ *${prefix}cmdlist*
+┃║⛇ *${prefix}anotar* <text>
+┃║⛇ *${prefix}notas*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.menufigu = `
-┏━━━━━━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙵𝚒𝚐𝚞𝚛𝚒𝚗𝚑𝚊𝚜
-┗━━━━━━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}s 
-┃
-┃۞ ${prefix}s2 
-┃
-┃۞ ${prefix}sfundo
-┃
-┃۞ ${prefix}stag
-┃
-┃۞ ${prefix}take
-┃
-┃۞ ${prefix}toimg
-┃
-┃۞ ${prefix}togif
-┃
-┃۞ ${prefix}attp
-┃
-┣━━━━━━━━━
-┃〘࿙ +18 ࿚〙
-┣━━━━━━━━━
-┃
-┃۞ ${prefix}scum
-┃
-┃۞ ${prefix}shentai
-┃
-┃۞ ${prefix}sfeetg
-┃
-┃۞ ${prefix}sbj
-┃
-┃۞ ${prefix}spussy
-┃
-┃۞ ${prefix}sneko
-┗━━━━━━━━━━━━━
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *FIGURINHAS* ❳═>
+┃║⛇ *${prefix}s*
+┃║⛇ *${prefix}s2*
+┃║⛇ *${prefix}sfundo*
+┃║⛇ *${prefix}stag*
+┃║⛇ *${prefix}take* <text>
+┃║⛇ *${prefix}toimg*
+┃║⛇ *${prefix}togif*
+┃║⛇ *${prefix}attp* <text>
+┃║
+┃║〘࿙ +18 ࿚〙
+┃║
+┃║⛇ *${prefix}scum*
+┃║⛇ *${prefix}shentai*
+┃║⛇ *${prefix}sfeetg*
+┃║⛇ *${prefix}sbj*
+┃║⛇ *${prefix}spussy*
+┃║⛇ *${prefix}sneko*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.menuutils = `
-┏━━━━━━━━━━━━
-┃𝙼𝚎𝚗𝚞𝚄𝚝𝚒𝚕𝚒𝚍𝚊𝚍𝚎𝚜
-┗━━━━━━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}anime 
-┃
-┃۞ ${prefix}play 
-┃
-┃۞ ${prefix}playvid 
-┃
-┃۞ ${prefix}gerarnick 
-┃
-┃۞ ${prefix}traduzir
-┃
-┃۞ ${prefix}stalkyt 
-┃
-┃۞ ${prefix}img 
-┃
-┃۞ ${prefix}upload
-┃
-┃۞ ${prefix}gerarcpf
-┃
-┃۞ ${prefix}ddd
-┗━━━━━━━━━━━━━
-
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *UTILIDADES* ❳═>
+┃║⛇ *${prefix}anime* <text>
+┃║⛇ *${prefix}play* <text>
+┃║⛇ *${prefix}playvid* <text>
+┃║⛇ *${prefix}gerarnick*
+┃║⛇ *${prefix}traduzir* <text>
+┃║⛇ *${prefix}stalkyt* <text>
+┃║⛇ *${prefix}img* <text>
+┃║⛇ *${prefix}upload*
+┃║⛇ *${prefix}gerarcpf*
+┃║⛇ *${prefix}ddd* <text>
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.menuent = `
-┏━━━━━━━━━━━━━━━
-┃𝙼𝚎𝚗𝚞𝙴𝚗𝚝𝚛𝚎𝚝𝚎𝚗𝚒𝚖𝚎𝚗𝚝𝚘
-┗━━━━━━━━━━━━━━━
-┏━━━━━━━━━━━━━
-┃۞ ${prefix}sorteio
-┃
-┃۞ ${prefix}amongus
-┃
-┃۞ ${prefix}gado
-┃
-┃۞ ${prefix}sn
-┃
-┃۞ ${prefix}chance
-┃
-┃۞ ${prefix}pau
-┃
-┃۞ ${prefix}gay
-┃
-┃۞ ${prefix}caracoroa
-┃
-┃۞ ${prefix}dado
-┃
-┃۞ ${prefix}plaq(até a 10)
-┃
-┃۞ ${prefix}death
-┃
-┃۞ ${prefix}gostosa
-┃
-┃۞ ${prefix}gostosa2
-┃
-┃۞ ${prefix}casal
-┃
-┃۞ ${prefix} abraço
-┃
-┃۞ ${prefix}chute
-┃
-┃۞ ${prefix}ship
-┃
-┃۞ ${prefix}carinho
-┃
-┃۞ ${prefix}piada
-┃
-┃۞ ${prefix}voz
-┃
-┃۞ ${prefix}padoru
-┗━━━━━━━━━━━━━
+┃╔═❲ *ENTRETENIMENTO* ❳═>
+┃║⛇ *${prefix}sorteio* <text>
+┃║⛇ *${prefix}amongus* <tag>
+┃║⛇ *${prefix}gado*
+┃║⛇ *${prefix}sn* <text>
+┃║⛇ *${prefix}chance* <text>
+┃║⛇ *${prefix}pau*
+┃║⛇ *${prefix}gay*
+┃║⛇ *${prefix}caracoroa*
+┃║⛇ *${prefix}dado*
+┃║⛇ *${prefix}plaq(até a 10)* <text>
+┃║⛇ *${prefix}death*
+┃║⛇ *${prefix}gostosa*
+┃║⛇ *${prefix}gostosa2*
+┃║⛇ *${prefix}casal*
+┃║⛇ *${prefix} abraço* <tag>
+┃║⛇ *${prefix}chute* <tag>
+┃║⛇ *${prefix}ship*
+┃║⛇ *${prefix}carinho* <tag>
+┃║⛇ *${prefix}piada*
+┃║⛇ *${prefix}voz*
+┃║⛇ *${prefix}padoru*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
 
 exports.allmenu = `
-◑ ━━━━━ ▣ ━━━━━ ◐
-    𝐂 𝐎 𝐌 𝐀 𝐍 𝐃 𝐎 𝐒 :
-◑ ━━━━━ ▣ ━━━━━ ◐
-
-▰▰▰▰▰▰▰▰
-𝘼𝙙𝙢𝙞𝙣𝙨
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}add 
-
-۞ ${prefix}ban 
-
-۞ ${prefix}antifake 
-
-۞ ${prefix}antilink 
-
-۞ ${prefix}antipalavrão
-
-۞ ${prefix}nsfw 
-
-۞ ${prefix}welkom  
-
-۞ ${prefix}contador 
-
-۞ ${prefix}checkativo 
-
-۞ ${prefix}topativos 
-
-۞ ${prefix}opentime 
-
-۞ ${prefix}closetime 
-
-۞ ${prefix}opengp 
-
-۞ ${prefix}closegp 
-
-۞ ${prefix}promote 
-
-۞ ${prefix}demote 
-
-۞ ${prefix}hidetag 
-
-۞ ${prefix}linkgp 
-
-۞ ${prefix}desc 
-
-۞ ${prefix}multiprefix
-
-۞ ${prefix}setdesc 
-
-۞ ${prefix}setname 
-
-۞ ${prefix}setpic 
-
-۞ ${prefix}roleta
-
-۞ ${prefix}mute
-
-۞ ${prefix}status
-
-۞ ${prefix}addblack
-
-۞ ${prefix}unblack
-
-۞ ${prefix}blacklist
-
-۞ ${prefix}addwhite 
-
-۞ ${prefix}delwhite 
-
-۞ ${prefix}whitelist 
-
-۞ ${prefix}delete
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝘿𝙤𝙣𝙤
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}block 
-
-۞ ${prefix}unblock 
-
-۞ ${prefix}blocklist 
-
-۞ ${prefix}gplist 
-
-۞ ${prefix}pvlist 
-
-۞ ${prefix}seradm 
-
-۞ ${prefix}sermembro 
-
-۞ ${prefix}leave 
-
-۞ ${prefix}join 
-
-۞ ${prefix}recado 
-
-۞ ${prefix}on 
-
-۞ ${prefix}off 
-
-۞ ${prefix}antipv
-
-۞ ${prefix}blockcmd
-
-۞ ${prefix}unblockcmd
-
-۞ ${prefix}cmdlist
-
-۞ ${prefix}antipv
-
-۞ ${prefix}anotar
-
-۞ ${prefix}notas
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙁𝙞𝙜𝙪𝙧𝙞𝙣𝙝𝙖𝙨
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}s 
-
-۞ ${prefix}s2 
-
-۞ ${prefix}sfundo
-
-۞ ${prefix}stag
-
-۞ ${prefix}take
-
-۞ ${prefix}toimg
-
-۞ ${prefix}togif
-
-۞ ${prefix}attp
-
-〘࿙ +18 ࿚〙
-
-۞ ${prefix}scum
-
-۞ ${prefix}shentai
-
-۞ ${prefix}sfeetg
-
-۞ ${prefix}sbj
-
-۞ ${prefix}spussy
-
-۞ ${prefix}sneko
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙐𝙩𝙞𝙡𝙨
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}anime 
-
-۞ ${prefix}play 
-
-۞ ${prefix}playvid 
-
-۞ ${prefix}gerarnick 
-
-۞ ${prefix}traduzir
-
-۞ ${prefix}stalkyt 
-
-۞ ${prefix}img 
-
-۞ ${prefix}upload
-
-۞ ${prefix}gerarcpf
-
-۞ ${prefix}ddd
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙀𝙣𝙩𝙧𝙚𝙩𝙚𝙣𝙞𝙢𝙚𝙣𝙩𝙤
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}sorteio
-
-۞ ${prefix}amongus
-
-۞ ${prefix}gado
-
-۞ ${prefix}sn
-
-۞ ${prefix}chance
-
-۞ ${prefix}pau
-
-۞ ${prefix}gay
-
-۞ ${prefix}caracoroa
-
-۞ ${prefix}dado
-
-۞ ${prefix}plaq(até a 10)
-
-۞ ${prefix}death
-
-۞ ${prefix}gostosa
-
-۞ ${prefix}gostosa2
-
-۞ ${prefix}casal
-
-۞ ${prefix} abraço
-
-۞ ${prefix}chute
-
-۞ ${prefix}ship
-
-۞ ${prefix}carinho
-
-۞ ${prefix}piada
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙂𝙖𝙡𝙚𝙧𝙞𝙖
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}loli 
-
-۞ ${prefix}neko 
-
-۞ ${prefix}ranime 
-
-۞ ${prefix}wallpaper 
-
-۞ ${prefix}yuri 
-
-۞ ${prefix}kimono
-
-۞ ${prefix}uwu
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙊𝙪𝙩𝙧𝙤𝙨𝘾𝙤𝙢𝙖𝙣𝙙𝙤𝙨
-▰▰▰▰▰▰▰▰
-
-۞ ${prefix}bug
-
-۞ ${prefix}avaliar
-
-۞ ${prefix}criador
-
-۞ ${prefix}learn
-
-• - - - - - - ☆- - - - - - •
-
-
-
-
-
-▰▰▰▰▰▰▰▰
-𝙉𝙨𝙛𝙬
-▰▰▰▰▰▰▰▰
-
-
-۞ ${prefix}ass 
-
-۞ ${prefix}bdsm 
-
-۞ ${prefix}blowjob 
-
-۞ ${prefix}cuckold 
-
-۞ ${prefix}cum 
-
-۞ ${prefix}cumslut 
-
-۞ ${prefix}doujin 
-
-۞ ${prefix}ero 
-
-۞ ${prefix}eroneko 
-
-۞ ${prefix}eroyuri 
-
-۞ ${prefix}feet 
-
-۞ ${prefix}femdom 
-
-۞ ${prefix}foxgirl 
-
-۞ ${prefix}futa 
-
-۞ ${prefix}gangbang 
-
-۞ ${prefix}glasses 
-
-۞ ${prefix}maid 
-
-۞ ${prefix}netorare 
-
-۞ ${prefix}orgy 
-
-۞ ${prefix}panties 
-
-۞ ${prefix}pussy 
-
-۞ ${prefix}rhentai
-
-۞ ${prefix}school 
-
-۞ ${prefix}solo 
-
-۞ ${prefix}succubus 
-
-۞ ${prefix}tentacles 
-
-۞ ${prefix}thighs 
-
-۞ ${prefix}trap 
-
-۞ ${prefix}ugly 
-
-۞ ${prefix}fantasy 
-
-۞ ${prefix}zettai 
-
-
-
-• - - - - - - ☆- - - - - - •
-
-
+┏━━━━❲ *XYz-BOTz* ❳━━━━━
+┃ OLÁ ${pushname}!! <3
+┗━━━━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *MENU GRUPO* ❳═>
+┃║⛇ *${prefix}add* <num>
+┃║⛇ *${prefix}ban* <num>
+┃║⛇ *${prefix}antifake*
+┃║⛇ *${prefix}antilink*
+┃║⛇ *${prefix}antipalavrão*
+┃║⛇ *${prefix}nsfw*
+┃║⛇ *${prefix}welkom*
+┃║⛇ *${prefix}contador* <num>
+┃║⛇ *${prefix}checkativo* <num>
+┃║⛇ *${prefix}topativos*
+┃║⛇ *${prefix}opentime*
+┃║⛇ *${prefix}closetime*
+┃║⛇ *${prefix}opengp*
+┃║⛇ *${prefix}closegp*
+┃║⛇ *${prefix}promote* <num>
+┃║⛇ *${prefix}demote* <num>
+┃║⛇ *${prefix}hidetag*
+┃║⛇ *${prefix}linkgp*
+┃║⛇ *${prefix}desc*
+┃║⛇ *${prefix}multiprefix*
+┃║⛇ *${prefix}setdesc* <text>
+┃║⛇ *${prefix}setname* <text>
+┃║⛇ *${prefix}setpic* <tag img>
+┃║⛇ *${prefix}roleta*
+┃║⛇ *${prefix}mute*
+┃║⛇ *${prefix}status*
+┃║⛇ *${prefix}addblack* <num>
+┃║⛇ *${prefix}unblack* <num>
+┃║⛇ *${prefix}blacklist*
+┃║⛇ *${prefix}addwhite* <num>
+┃║⛇ *${prefix}delwhite*
+┃║⛇ *${prefix}whitelist*
+┃║⛇ *${prefix}delete* <tag img>
+┃║⛇ *${prefix}listadmins
+┃╚══════════════════════
+┣━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *DONO* ❳═>
+┃║⛇ *${prefix}block* <num>
+┃║⛇ *${prefix}unblock* <num>
+┃║⛇ *${prefix}blocklist*
+┃║⛇ *${prefix}gplist*
+┃║⛇ *${prefix}pvlist*
+┃║⛇ *${prefix}seradm*
+┃║⛇ *${prefix}sermembro*
+┃║⛇ *${prefix}leave*
+┃║⛇ *${prefix}join*
+┃║⛇ *${prefix}recado*
+┃║⛇ *${prefix}on*
+┃║⛇ *${prefix}off*
+┃║⛇ *${prefix}antipv* <on/off>
+┃║⛇ *${prefix}blockcmd* <cmd>
+┃║⛇ *${prefix}unblockcmd* <cmd>
+┃║⛇ *${prefix}cmdlist*
+┃║⛇ *${prefix}anotar* <text>
+┃║⛇ *${prefix}notas*
+┃╚══════════════════════
+┣━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *FIGURINHAS* ❳═>
+┃║⛇ *${prefix}s*
+┃║⛇ *${prefix}s2*
+┃║⛇ *${prefix}sfundo*
+┃║⛇ *${prefix}stag*
+┃║⛇ *${prefix}take* <text>
+┃║⛇ *${prefix}toimg*
+┃║⛇ *${prefix}togif*
+┃║⛇ *${prefix}attp* <text>
+┃║
+┃║〘࿙ +18 ࿚〙
+┃║
+┃║⛇ *${prefix}scum*
+┃║⛇ *${prefix}shentai*
+┃║⛇ *${prefix}sfeetg*
+┃║⛇ *${prefix}sbj*
+┃║⛇ *${prefix}spussy*
+┃║⛇ *${prefix}sneko*
+┃╚══════════════════════
+┃╔═❲ *UTILIDADES* ❳═>
+┃║⛇ *${prefix}anime* <text>
+┃║⛇ *${prefix}play* <text>
+┃║⛇ *${prefix}playvid* <text>
+┃║⛇ *${prefix}gerarnick*
+┃║⛇ *${prefix}traduzir* <text>
+┃║⛇ *${prefix}stalkyt* <text>
+┃║⛇ *${prefix}img* <text>
+┃║⛇ *${prefix}upload*
+┃║⛇ *${prefix}gerarcpf*
+┃║⛇ *${prefix}ddd* <text>
+┃╚══════════════════════
+┃╔═❲ *ENTRETENIMENTO* ❳═>
+┃║⛇ *${prefix}sorteio* <text>
+┃║⛇ *${prefix}amongus* <tag>
+┃║⛇ *${prefix}gado*
+┃║⛇ *${prefix}sn* <text>
+┃║⛇ *${prefix}chance* <text>
+┃║⛇ *${prefix}pau*
+┃║⛇ *${prefix}gay*
+┃║⛇ *${prefix}caracoroa*
+┃║⛇ *${prefix}dado*
+┃║⛇ *${prefix}plaq(até a 10)* <text>
+┃║⛇ *${prefix}death*
+┃║⛇ *${prefix}gostosa*
+┃║⛇ *${prefix}gostosa2*
+┃║⛇ *${prefix}casal*
+┃║⛇ *${prefix} abraço* <tag>
+┃║⛇ *${prefix}chute* <tag>
+┃║⛇ *${prefix}ship*
+┃║⛇ *${prefix}carinho* <tag>
+┃║⛇ *${prefix}piada*
+┃║⛇ *${prefix}voz*
+┃║⛇ *${prefix}padoru*
+┃╚══════════════════════
+┃╔═❲ *GALERIA* ❳═>
+┃║⛇ *${prefix}loli*
+┃║⛇ *${prefix}neko*
+┃║⛇ *${prefix}ranime*
+┃║⛇ *${prefix}wallpaper*
+┃║⛇ *${prefix}yuri*
+┃║⛇ *${prefix}kimono*
+┃║⛇ *${prefix}uwu*
+┃╚══════════════════════
+┃╔═❲ *OUTRAS OPÇÕES* ❳═>
+┃║⛇ *${prefix}bug* <text>
+┃║⛇ *${prefix}avaliar*
+┃║⛇ *${prefix}criador*
+┃║⛇ *${prefix}learn*
+┃╚══════════════════════
+┃╔═❲ *NSFW* ❳═>
+┃║⛇ *${prefix}ass*
+┃║⛇ *${prefix}bdsm*
+┃║⛇ *${prefix}blowjob*
+┃║⛇ *${prefix}cuckold*
+┃║⛇ *${prefix}cum*
+┃║⛇ *${prefix}cumslut*
+┃║⛇ *${prefix}doujin*
+┃║⛇ *${prefix}ero*
+┃║⛇ *${prefix}eroneko*
+┃║⛇ *${prefix}eroyuri*
+┃║⛇ *${prefix}feet*
+┃║⛇ *${prefix}femdom*
+┃║⛇ *${prefix}foxgirl*
+┃║⛇ *${prefix}futa*
+┃║⛇ *${prefix}gangbang*
+┃║⛇ *${prefix}glasses*
+┃║⛇ *${prefix}maid*
+┃║⛇ *${prefix}netorare 
+┃║⛇ *${prefix}orgy*
+┃║⛇ *${prefix}panties*
+┃║⛇ *${prefix}pussy*
+┃║⛇ *${prefix}rhentai*
+┃║⛇ *${prefix}school*
+┃║⛇ *${prefix}solo*
+┃║⛇ *${prefix}succubus*
+┃║⛇ *${prefix}tentacles*
+┃║⛇ *${prefix}thighs*
+┃║⛇ *${prefix}trap*
+┃║⛇ *${prefix}ugly*
+┃║⛇ *${prefix}fantasy*
+┃║⛇ *${prefix}zettai*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━━━
+┃╔═❲ *<3* ❳═>
+┃║⛇ *Xʏ*
+┃╚══════════════════════
+┗━━━━━━━━━━━━━━━━━━━━━━━
 `
